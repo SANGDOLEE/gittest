1afa5abad8ca316e65d7d7b8dba2a39e99e8a1c4
xkdxh625@naver.com <xkdxh625@naver.com>
2024-03-14 19:56:29 +0900

    commit

---
+++ test/test/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 이상도 on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        // 첫번째 커밋
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 이상도 on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
